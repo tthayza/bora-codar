a3ab3849c0f447fe80675e1f6ff9bbb629820f05
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2023-03-22 12:28:34 -0300

    Fix the sun position in weather page

--- 10. Weather-Page/src/components/SunsetHistoric/index.jsx
+++ 10. Weather-Page/src/components/SunsetHistoric/index.jsx
@@ -15,6 +15,10 @@ function SunsetHistoric(props) {
   const hoursSunset = new Date(props.weatherInfo?.daily?.sunset[props.parameterIndexSun]).getHours()
   const minutesSunset = new Date(props.weatherInfo?.daily?.sunset[props.parameterIndexSun]).getMinutes()
 
+  const sunriseTime = hoursSunrise + ':' + minutesSunrise
+  const sunsetTime = hoursSunset + ':' + minutesSunset
+
+  const sunPosition = hours < 6 ? 0 : hours > 18 ? 100 : (hours - 6) * 100 / 12
 
 
   return(
@@ -28,7 +32,7 @@ function SunsetHistoric(props) {
         </h2>
       </div>
       <div className="sun-chart-wrapper">
-        <div className="sun-chart">
+        <div className="sun-chart"  style={{ "--pos-x": sunPosition }}>
           <div className="chart ">
             <img src={chartSun} alt="" />
           </div>
@@ -36,8 +40,8 @@ function SunsetHistoric(props) {
         </div>
       </div>
       <div className="time-sun">
-            <time className="sunrise">{hoursSunrise}:{minutesSunrise}</time>
-            <time className="sunset">{hoursSunset}:{minutesSunset}</time>
+            <time className="sunrise">{sunriseTime}</time>
+            <time className="sunset">{sunsetTime}</time>
 
           </div>
     </div>
