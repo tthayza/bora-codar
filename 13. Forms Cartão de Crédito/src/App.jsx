34cf0c25422ea358e6177af8d86a0ba7e1da5147
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2023-04-02 18:46:20 -0300

    Connect inputs with card data

--- 13. Forms Cartão de Crédito/src/App.jsx
+++ 13. Forms Cartão de Crédito/src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import shield from './assets/shield.svg'
 import CreditCard from './components/CreditCard'
 import './styles/global.css'
 
 const App = () => {
+  const [cardNumber, setCardNumber] = useState('')
+  const [cardHolderName, setCardHolderName] = useState('')
+  const [cardValidity, setCardValidity] = useState('')
+  const [cardCodeCard, setCardCodeCard] = useState('')
+
   return (
     <div className="app">
       <div className="container">
@@ -12,32 +17,62 @@ const App = () => {
             <div className="form">
               <div className=" entry">
                 <label htmlFor="card-number">Número do cartão</label>
-                <input type="text" id="card-number" />
+                <input
+                  type="text"
+                  id="card-number"
+                  onChange={e => setCardNumber(e.target.value)}
+                />
               </div>
               <div className=" entry">
                 <label htmlFor="card-holder-name">Nome do titular</label>
-                <input type="text" id="card-holder-name" />
+                <input
+                  type="text"
+                  id="card-holder-name"
+                  onChange={e =>
+                    setCardHolderName(e.target.value.toUpperCase())
+                  }
+                />
               </div>
               <div className="data-card">
                 <div className="entry">
                   <label htmlFor="validity">Validade</label>
-                  <input type="text" name="" id="validity" />
+                  <input
+                    type="text"
+                    name=""
+                    id="validity"
+                    onChange={e => setCardValidity(e.target.value)}
+                  />
                 </div>
                 <div className="entry">
                   <div className="entry">
                     <label htmlFor="code-card">CVV</label>
                   </div>
-                  <input type="password" name="" id="code-card" />
+                  <input
+                    type="password"
+                    name=""
+                    id="code-card"
+                    onChange={e => setCardCodeCard(e.target.value)}
+                  />
                 </div>
               </div>
             </div>
           </div>
           <div className="right side">
-            <CreditCard />
+            <CreditCard
+              cardNumber={cardNumber}
+              cardHolderName={cardHolderName}
+              cardValidity={cardValidity}
+              cardCodeCard={cardCodeCard}
+            />
 
             <div className="text-footer">
-              <img src={shield} alt="" />
-              <h2>Seus dados estão seguros</h2>
+              <div className="text">
+                <img src={shield} alt="" />
+                <h2>Seus dados estão seguros</h2>
+              </div>
+              <div className="btn-turn">
+                <button className="btn-turning"></button>
+              </div>
             </div>
           </div>
         </div>
--- 13. Forms Cartão de Crédito/src/components/CreditCard/index.jsx
+++ 13. Forms Cartão de Crédito/src/components/CreditCard/index.jsx
@@ -1,34 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import contactPayment from '../../assets/contact-payment.svg'
 import elo from '../../assets/elo.svg'
+import mastercard from '../../assets/mastercard.svg'
+import visa from '../../assets/visa.svg'
 import './styles.css'
-//n consegui importar o simbolo de contato por pagamento
-const CreditCard = () => {
-  // const flags = {
-  //   {
-  //     mastercard: {mastercard}
-  //   },
-  //   {
-  //     visa: {visa}
-  //   },
-  //   { elo: {elo}
-  //   }
-  //   4 visa
-  // 2 ou 5 mastercard
-  // 6 elo
-  // }
+
+const CreditCard = props => {
+  const [showBack, setShowBack] = useState(false)
+  function flipCard() {
+    setShowBack(!showBack)
+  }
+  const { cardNumber, cardHolderName, cardValidity, cardCodeCard } = props
+
+  const setCurrentFlag = currentFlag => {
+    if (currentFlag == '5' || currentFlag == '2') {
+      return mastercard
+    } else if (currentFlag == '6') {
+      return visa
+    }
+    return elo
+  }
+  console.log(cardCodeCard)
+  const formatedValidity = date => {
+    const formatedDate = date.padEnd(4, '•').match(/.{1,2}/g)
+    return (
+      <h1>
+        {formatedDate[0]}/{formatedDate[1]}
+      </h1>
+    )
+  }
   return (
-    <div className="credit-card">
-      <div className="logo">
-        <img src={elo} alt="" />
-        <img src={contactPayment} alt="" />
+    <div
+      className={`credit-card  ${showBack ? 'flip' : ''}`}
+      onClick={flipCard}
+    >
+      <div className=" card-front">
+        <div className="logo">
+          <img src={setCurrentFlag(cardNumber[0])} alt="" />
+          <img src={contactPayment} alt="" />
+        </div>
+        <div className="number-card">
+          {cardNumber
+            .padEnd(16, '•')
+            .match(/.{1,4}/g)
+            .map((subtring, index) => (
+              <h1 key={index}>{subtring}</h1>
+            ))}
+        </div>
+        <div className="name-expiry">
+          <h1>{cardHolderName || 'Seu nome aqui'} </h1>
+          {formatedValidity(cardValidity)}
+        </div>
       </div>
-      <div className="number-card">
-        <h1>4215 4545 5454 7444</h1>
-      </div>
-      <div className="name-expiry">
-        <h1>Seu nome aqui</h1>
-        <h1>00 / 00</h1>
+      <div className=" card-back">
+        <div className="black-belt"></div>
+        <div className="cvv-area">
+          <p className="code-area">{cardCodeCard}</p>
+          <p className="cvv">CVV</p>
+        </div>
       </div>
     </div>
   )
