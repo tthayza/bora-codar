50a3f8e7544b1247d3421be970b11ff2cbc66d08
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2023-04-10 19:55:02 -0300

    Adds functionality to the upload component project

--- 14. Upload Component/src/App.jsx
+++ 14. Upload Component/src/App.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import upload from './assets/upload.svg'
 import File from './components/File'
 
 function App() {
+  const [files, setFiles] = useState([{ name: 'novo.txt', size: '44 MB' }])
+  const addNewFile = () => {
+    // const newList = [
+    //   { name: 'novoNovo.txt', size: '488 MB' },
+    //   ...files
+    // ].reverse()
+    setFiles(prev =>
+      [...prev, { name: 'novoNovo.txt', size: '488 MB' }].reverse()
+    )
+  }
   return (
     <div className="app">
       <div className="container">
-        <div className="import-area">
+        <div className="import-area" onClick={() => addNewFile()}>
           <img src={upload} alt="ícone de upload" />
           <h2>Importe seus arquivos</h2>
           <h3>Arraste ou clique para fazer upload</h3>
         </div>
         <div className="file-status">
-          <File name="Scann_158.pdf" size="74 MB" />
-          <File name="Scann_158.pdf" size="74 MB" />
-          <File name="Scann_158.pdf" size="74 MB" />
-          <File name="Scann_158.pdf" size="74 MB" />
-          <File name="Scann_158.pdf" size="74 MB" />
-          <File name="Scann_158.pdf" size="74 MB" />
+          {files.map((file, index) => (
+            <File key={index} name={file.name + index} size={file.size} />
+          ))}
         </div>
       </div>
     </div>
--- 14. Upload Component/src/components/File/index.jsx
+++ 14. Upload Component/src/components/File/index.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import cancel from '../../assets/cancel.svg'
-import file from '../../assets/uploaded-file.svg'
+import progressFile from '../../assets/progress-file.svg'
+import fileUploaded from '../../assets/uploaded-file.svg'
 import './styles.css'
 
 function File(props) {
+  const [currentFile, setCurrentFile] = useState(progressFile)
   const { name, size } = props
+  const [percentage, setPercentage] = useState(9)
+  const [visibility, setVisibility] = useState('visible')
+  const [color, setColor] = useState('#9892a6')
+  const [bgColor, setBgColor] = useState('crimson')
+  useEffect(() => {
+    if (percentage < 100) {
+      const intervalo = setInterval(() => {
+        setPercentage(prev => prev + 1)
+      }, 90)
+      return () => clearInterval(intervalo)
+    } else {
+      setVisibility('hidden')
+      setColor('green')
+      setBgColor('green')
+      setCurrentFile(fileUploaded)
+    }
+  }, [percentage])
+  const [display, setDisplay] = useState('flex')
+
   return (
-    <div className="file">
+    <div className="file" style={{ display: display }}>
       <div className="image">
-        <img src={file} alt="" />
+        <img src={currentFile} alt="" />
       </div>
       <div className="infos">
         <div className="top">
@@ -16,15 +37,21 @@ function File(props) {
             <h2 className="name"> {name}</h2>
             <h3 className="size">{size}</h3>
           </div>
-          <div className="icon">
-            <img src={cancel} alt="" />
+          <div className="icon" onClick={() => setDisplay('none')}>
+            <img src={cancel} alt="" style={{ visibility: visibility }} />
           </div>
         </div>
         <div className="bottom">
           <div className="progress-bar">
-            <div className="bar"> </div>
-            <div className="progress"></div>
-            <div className="current-status">44%</div>
+            <div className="bar">
+              <div
+                className="progress"
+                style={{ width: percentage * 3, backgroundColor: bgColor }}
+              ></div>
+            </div>
+          </div>
+          <div className="current-status">
+            <p style={{ color: color }}>{percentage}%</p>
           </div>
         </div>
       </div>
