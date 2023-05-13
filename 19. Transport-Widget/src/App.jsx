42ee5ee154530d846f2d8d5bf066bd3fcd755dfb
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2023-05-12 23:28:58 -0300

    Iniatialize transport widget project layout

--- 19. Transport-Widget/src/App.jsx
+++ 19. Transport-Widget/src/App.jsx
@@ -1,34 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import car from './assets/car.svg'
+import driver from './assets/driver.png'
+import star from './assets/star.svg'
+import './styles/global.css'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className="container">
+        <div className="bar">
+          <div></div>
+        </div>
+        <div className="header">
+          <h1>
+            Encontre <span>Boris</span> no local de partida
+          </h1>
+          <p>Chega em 3 minutos (800 metros)</p>
+        </div>
+        <div className="infos">
+          <div className="driver-id">
+            <div className="driver-infos">
+              <div className="driver-data">
+                <div className="stars">
+                  <img src={star} alt="" />
+                  <h2> 5.0 </h2>
+                </div>
+                <div className="name-driver">
+                  <h2>Boris C.</h2>
+                </div>
+              </div>
+            </div>
+            <div className="driver-img">
+              <img src={driver} alt="" />
+            </div>
+            <div className="car-img">
+              <img src={car} alt="" />
+            </div>
+          </div>
+          <div className="car-id">
+            <h1>BCD0D19</h1>
+            <h2>Honda Civic Rosa</h2>
+          </div>
+        </div>
+        <div className="send-message">
+          <input type="text" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
