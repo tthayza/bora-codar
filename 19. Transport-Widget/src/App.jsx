bedeacc2321ba1aa3eb01ec781ef1b4a23e44463
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2023-05-14 15:27:53 -0300

    Finish the style in transport widget project

--- 19. Transport-Widget/src/App.jsx
+++ 19. Transport-Widget/src/App.jsx
@@ -1,5 +1,7 @@
+import call from './assets/call.svg'
 import car from './assets/car.svg'
 import driver from './assets/driver.png'
+import shield from './assets/shield.svg'
 import star from './assets/star.svg'
 import './styles/global.css'
 function App() {
@@ -28,11 +30,13 @@ function App() {
                 </div>
               </div>
             </div>
-            <div className="driver-img">
-              <img src={driver} alt="" />
-            </div>
-            <div className="car-img">
-              <img src={car} alt="" />
+            <div className="imgs">
+              <div className="driver-img">
+                <img src={driver} alt="" />
+              </div>
+              <div className="car-img">
+                <img src={car} alt="" />
+              </div>
             </div>
           </div>
           <div className="car-id">
@@ -41,9 +45,15 @@ function App() {
           </div>
         </div>
         <div className="send-message">
-          <input type="text" />
-          <img src="" alt="" />
-          <img src="" alt="" />
+          <input type="text" placeholder="Envie uma mensagem para Boris..." />
+          <div className="icons">
+            <img className="icon" src={call} alt="ícone de telefone" />
+            <img
+              className="icon"
+              src={shield}
+              alt="ícone de escudo com uma cruz dentro"
+            />
+          </div>
         </div>
       </div>
     </div>
