c4eb64b21354b54f8ded465fe56e098e7f6708d9
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2023-06-23 20:05:38 -0300

    Add player component

--- 25. VR-Player/src/App.jsx
+++ 25. VR-Player/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         <aside className="flex">
           <SideMenu />
         </aside>
-        <main>
+        <main className="flex">
           <Header />
 
           <Player />
--- 25. VR-Player/src/components/Player/index.jsx
+++ 25. VR-Player/src/components/Player/index.jsx
@@ -1,3 +1,36 @@
+import { useState } from 'react'
+import screen from '../../assets/Screen.png'
+import './style.css'
 export const Player = () => {
-  return <div></div>
+  const [display, setDisplay] = useState('none')
+  const [displayImg, setDisplayImg] = useState('block')
+  const displayHandler = () => {
+    if (display === 'none') {
+      setDisplay('block')
+      setDisplayImg('none')
+    } else {
+      setDisplay('none')
+      setDisplayImg('block')
+    }
+  }
+
+  return (
+    <div className="player">
+      <img
+        src={screen}
+        onClick={() => displayHandler()}
+        style={{ display: displayImg }}
+      ></img>
+      <iframe
+        onClick={() => displayHandler()}
+        style={{ display: display }}
+        loading="eager"
+        src="https://www.youtube.com/embed/uL6nu0-zhu0?controls=0"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+    </div>
+  )
 }
