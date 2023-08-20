6b5e7383ac6d65c63dc68a4b8de159303ed0e649
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2023-08-20 00:52:56 -0300

    Apply TMDB API

--- 30. Movie-Recommendation/src/App.jsx
+++ 30. Movie-Recommendation/src/App.jsx
@@ -1,20 +1,74 @@
+/* eslint-disable no-unused-vars */
 import { Lightning } from '@phosphor-icons/react'
+import { useEffect, useState } from 'react'
 import logo from './assets/logo.svg'
-import Card from './components/Card'
+import Card from './components/Card/index'
 import './styles/global.css'
 function App() {
+  const [data, setData] = useState({})
+  const [best3Movies, setBest3Movies] = useState([])
+
+  useEffect(() => {
+    async function getMovies() {
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`
+        }
+      }
+      try {
+        const response = await fetch(
+          'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',
+          options
+        ).then((response) => response.json())
+
+        setData(response)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    getMovies()
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  useEffect(() => {
+    if (data.results) updateRecommendation()
+  }, [data])
+
+  function updateRecommendation() {
+    const results = data.results
+    const best3Movies = selectPopularMovies(results)
+    setBest3Movies(best3Movies)
+  }
+
+  const selectPopularMovies = (results) => {
+    const randomPosition = () => Math.floor(Math.random() * results.length)
+
+    let selectedVideos = new Set()
+    while (selectedVideos.size < 3) {
+      selectedVideos.add(results[randomPosition()].id)
+    }
+
+    const currentMovies = data.results.filter((movie) =>
+      [...selectedVideos].includes(movie.id)
+    )
+    return currentMovies
+  }
   return (
     <div className="app">
       <div className="container">
         <header className="header">
           <img src={logo} alt="logomarca" />
-          <button>
+          <button onClick={updateRecommendation}>
             Nova recomendação
             <Lightning />
           </button>
         </header>
         <main className="main">
-          <Card />
+          {best3Movies.map((movie) => {
+            return <Card key={movie.id} movie={movie} />
+          })}
         </main>
       </div>
     </div>
--- 30. Movie-Recommendation/src/components/Card/index.jsx
+++ 30. Movie-Recommendation/src/components/Card/index.jsx
@@ -1,29 +1,71 @@
 import { CalendarBlank, Clock, PlayCircle, Star } from '@phosphor-icons/react'
-import poster from '../../assets/poster.svg'
+import { useEffect, useState } from 'react'
 import './style.css'
 
-const Card = () => {
+const Card = (props) => {
+  const { movie } = props
+  const [movieDetails, setMovieDetails] = useState({})
+  const [movieInfos, setMovieInfos] = useState({})
+  useEffect(() => {
+    async function getMoreMovieInfo(id) {
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`
+        }
+      }
+      try {
+        const data = await fetch(
+          `https://api.themoviedb.org/3/movie/${id}?language=en-US`,
+          options
+        ).then((response) => response.json())
+        const infos = await fetch(
+          `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+          options
+        ).then((response) => response.json())
+        setMovieInfos(infos)
+        setMovieDetails(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    getMoreMovieInfo(movie.id)
+  }, [movie.id])
+
+  function watchTrailer() {
+    window.open(`https://www.youtube.com/watch?v=${movieInfos.results[0].key}`)
+  }
   return (
     <div className="card">
       <header>
-        <h2>Barbie</h2>
+        <h2>{movie.title}</h2>
         <span className="stars">
-          <Star color="yellow" /> 4.9
+          <Star color="yellow" /> {movie.vote_average}
         </span>
       </header>
       <main>
-        <img src={poster} alt="" />
+        <img
+          width="194"
+          src={`https://image.tmdb.org/t/p/w500/` + movie.poster_path}
+          alt=""
+        />
       </main>
       <footer>
         <section className="movie-infos">
           <span className="duration">
-            <Clock /> 1:54:00
+            <Clock />{' '}
+            {`${Math.floor(movieDetails.runtime / 60)}:${
+              movieDetails.runtime % 60 < 10
+                ? '0' + (movieDetails.runtime % 60)
+                : movieDetails.runtime % 60
+            }:00 `}
           </span>
           <span className="">
-            <CalendarBlank /> 2023
+            <CalendarBlank /> {movie.release_date.slice(0, 4)}
           </span>
         </section>
-        <button>
+        <button onClick={() => watchTrailer(movie.id)}>
           <PlayCircle /> Assistir trailer
         </button>
       </footer>
